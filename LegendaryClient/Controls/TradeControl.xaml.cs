012d751a269f29430d0c0fc88f172158d89b7b74
eddy5641 <your_email@example.com>
2014-05-09 15:54:53 -0700

    shi

---
+++ LegendaryClient/Controls/TradeControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LegendaryClient.Logic;
+using System.Windows;
+using System.Windows.Controls;
+using PVPNetConnect.RiotObjects.Platform.Trade;
+
+namespace LegendaryClient.Controls
+{
+    /// <summary>
+    /// Interaction logic for TradeControl.xaml
+    /// </summary>
+    public partial class TradeControl : UserControl
+    {
+        public TradeControl()
+        {
+            InitializeComponent();
+        }
+
+        private async void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            TradeContractDTO TradeDTO = this.Tag as TradeContractDTO;
+            await Client.PVPNet.AcceptTrade(TradeDTO.RequesterInternalSummonerName, (int)TradeDTO.RequesterChampionId);
+        }
+
+        private async void DeclineButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Client.PVPNet.DeclineTrade();
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+}
