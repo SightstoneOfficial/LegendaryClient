0ad257777a73e35ac5e423753c2e5e10efb1bad2
Dispersia <dispersias@gmail.com>
2014-10-08 01:53:51 -0700

    Last one I swear!
    Reverting latestversion to prepare for next patch.

--- LegendaryClient/Windows/PatcherPage.xaml.cs
+++ LegendaryClient/Windows/PatcherPage.xaml.cs
@@ -209,7 +209,7 @@ namespace LegendaryClient.Windows
                 //bool RetrieveCurrentInstallation = false; //just added this?
                 WebClient UpdateClient = new WebClient();
                 string Release = UpdateClient.DownloadString("http://l3cdn.riotgames.com/releases/live/projects/lol_air_client/releases/releaselisting");
-                string LatestVersion = Release.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)[1]; 
+                string LatestVersion = Release.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)[0]; 
                 
                 if (AirVersion != LatestVersion)
                 {
