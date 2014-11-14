c3c6f5ac483adb7bc6e68e566168323699ab4109
eddy5641 <eddy5641@gmail.com>
2014-11-14 15:36:59 -0800

    Swich PC

---
+++ LegendaryClient/Logic/GameClientSettings/General.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryClient.Logic.GameClientSettings
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class General : Attribute
+    {
+        public string name { get; set; }
+        public bool isGeneral { get; set; }
+        public General(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ LegendaryClient/Logic/GameClientSettings/LOLGCSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryClient.Logic.GameClientSettings
+{
+    public class LOLGCSettings
+    {
+        //General
+        [General("UserSetResolution")]
+        public int userSetResolution { get; set; }
+        [General("bindSysKeys")]
+        public int bindSysKeys { get; set; }
+        [General("SnapCameraOnRespawn")]
+        public int snapCameraOnRespawn { get; set; }
+        [General("OSXMouseAcceleration")]
+        public int OSXMouseAcceleration { get; set; }
+        [General("AutoAcquireTarget")]
+        public int autoAcquireTarget { get; set; }
+        [General("EnableLightFx")]
+        public int enableLightFx { get; set; }
+        [General("WindowMode")]
+        public int windowMode { get; set; }
+        [General("ShowTurretRangeIndicators")]
+        public int showTurrentRangeIndicators { get; set; }
+        [General("PredictMovement")]
+        public int predictMovement { get; set; }
+        [General("WaitForVerticalSync")]
+        public int waitForVerticalSync { get; set; }
+        [General("Colors")]
+        public int colors { get; set; }
+        [General("Height")]
+        public int height { get; set; }
+        [General("Width")]
+        public int width { get; set; }
+    }
+}
