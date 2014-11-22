143418c7b966f2db12b14f89adc85f185b5dccc4
eddy5641 <eddy5641@gmail.com>
2014-11-21 18:53:26 -0800

    switch to other pc

---
+++ ReplayRecorder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplayRecorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Unable to record correctly. Please restart the program. Press any key to exit.");
+                Console.ReadKey();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
