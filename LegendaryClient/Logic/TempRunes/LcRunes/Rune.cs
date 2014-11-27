9d0538fa94c2d216992aa861fd017d4255bbf969
eddy5641 <eddy5641@gmail.com>
2014-11-26 23:24:06 -0800

    Start Local Work + Delete useless stuff

---
+++ LegendaryClient/Logic/TempRunes/LcRunes/Rune.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendaryClient.Logic.TempRunes.LcRunes
+{
+    public abstract class Rune
+    {
+        public abstract string RuneType { get; }
+        public abstract RuneStats Stats { get; }
+    }
+}
