a63eddbfa168bd5ad866307ea83ca95b7c0c7975
RoachxD <roachxxxx@gmail.com>
2014-12-05 08:04:06 +0200

    Some update  - Removed unused References  - Removed all the "already specified" Types

--- LegendaryClientMLaunch/App.xaml.cs
+++ LegendaryClientMLaunch/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿#region
 
-using System.Windows;
+
 
 #endregion
 
@@ -9,7 +9,7 @@ namespace LegendaryClientMLaunch
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
