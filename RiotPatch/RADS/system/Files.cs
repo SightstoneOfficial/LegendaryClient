5b43cc3bc6b85c5b77728dfd2fca8036e3f90f92
RoachxD <roachxxxx@gmail.com>
2014-12-05 08:28:59 +0200

    Cleaner, smoother.. and so on..  - Removed unused Reference  - Tweaked, cleaned, re-indented the code

--- RiotPatch/RADS/system/Files.cs
+++ RiotPatch/RADS/system/Files.cs
@@ -1,37 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region
+
+using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+#endregion
 
 namespace RiotPatch.RADS.system
 {
     public class Files
     {
-        public static void EasyDownloader(string MainDownloadLocation, bool disableP2P = true, string Locale = "ko_KR", string region = "na", string Airproject = "lol_air_client", string GameProject = "lol_game_client_sln")
+        public static void EasyDownloader(string mainDownloadLocation, bool disableP2P = true, string locale = "ko_KR",
+            string region = "na", string airproject = "lol_air_client", string gameProject = "lol_game_client_sln")
         {
-            var file = File.Create(Path.Combine(MainDownloadLocation, "system", "launcher.cfg"));
+            FileStream file = File.Create(Path.Combine(mainDownloadLocation, "system", "launcher.cfg"));
             file.Close();
             file.Dispose();
-            StreamWriter writer = new StreamWriter(Path.Combine(MainDownloadLocation, "system", "launcher.cfg"));
-            writer.Write(string.Format(
-                "airConfigProject = lol_air_client_config_{0} {1} airProject = {2} {1} gameProject = {3} {1} installation_id = {4}", 
-                region, Environment.NewLine, Airproject, GameProject, "N159hA=="));
+            var writer = new StreamWriter(Path.Combine(mainDownloadLocation, "system", "launcher.cfg"));
+            writer.Write(
+                "airConfigProject = lol_air_client_config_{0} {1} airProject = {2} {1} gameProject = {3} {1} installation_id = {4}",
+                region, Environment.NewLine, airproject, gameProject, "N159hA==");
             writer.Close();
 
-            file = File.Create(Path.Combine(MainDownloadLocation, "system", "locale.cfg"));
-            writer = new StreamWriter(Path.Combine(MainDownloadLocation, "system", "locale.cfg"));
-            writer.Write("locale = " + Locale);
+            File.Create(Path.Combine(mainDownloadLocation, "system", "locale.cfg"));
+            writer = new StreamWriter(Path.Combine(mainDownloadLocation, "system", "locale.cfg"));
+            writer.Write("locale = " + locale);
             writer.Close();
 
-            file = File.Create(Path.Combine(MainDownloadLocation, "system", "system.cfg"));
-            writer = new StreamWriter(Path.Combine(MainDownloadLocation, "system", "system.cfg"));
-            writer.Write("DownloadPath = /releases/live" + Environment.NewLine+ "DownloadURL = l3cdn.riotgames.com" + Environment.NewLine + "Region = " + region.ToUpper());
+            File.Create(Path.Combine(mainDownloadLocation, "system", "system.cfg"));
+            writer = new StreamWriter(Path.Combine(mainDownloadLocation, "system", "system.cfg"));
+            writer.Write("DownloadPath = /releases/live" + Environment.NewLine + "DownloadURL = l3cdn.riotgames.com" +
+                         Environment.NewLine + "Region = " + region.ToUpper());
             writer.Close();
 
-            file = File.Create(Path.Combine(MainDownloadLocation, "system", "user.cfg"));
-            writer = new StreamWriter(Path.Combine(MainDownloadLocation, "system", "user.cfg"));
+            File.Create(Path.Combine(mainDownloadLocation, "system", "user.cfg"));
+            writer = new StreamWriter(Path.Combine(mainDownloadLocation, "system", "user.cfg"));
             writer.Write("disableP2P = " + disableP2P);
             writer.Close();
         }
