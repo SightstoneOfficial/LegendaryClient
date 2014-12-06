89d4330d7207fec12b1559d29331977105e6a1b5
RoachxD <roachxxxx@gmail.com>
2014-12-06 09:28:06 +0200

    Strange

---
+++ ReplayRecorder/TeamId.cs
@@ -0,0 +1,17 @@
+﻿#region
+
+using System;
+using RtmpSharp.IO;
+
+#endregion
+
+namespace LegendaryClient.Logic.Replays
+{
+    [Serializable]
+    [SerializedName("com.riotgames.team.TeamId")]
+    public class TeamId
+    {
+        [SerializedName("fullId")]
+        public String FullId { get; set; }
+    }
+}
---
+++ ReplayRecorder/TeamInfo.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System;
+using RtmpSharp.IO;
+
+#endregion
+
+namespace LegendaryClient.Logic.Replays
+{
+    [Serializable]
+    [SerializedName("com.riotgames.team.TeamInfo")]
+    public class TeamInfo
+    {
+        [SerializedName("secondsUntilEligibleForDeletion")]
+        public Double SecondsUntilEligibleForDeletion { get; set; }
+
+        [SerializedName("memberStatusString")]
+        public String MemberStatusString { get; set; }
+
+        [SerializedName("tag")]
+        public String Tag { get; set; }
+
+        [SerializedName("name")]
+        public String Name { get; set; }
+
+        [SerializedName("memberStatus")]
+        public String MemberStatus { get; set; }
+
+        [SerializedName("teamId")]
+        public TeamId TeamId { get; set; }
+    }
+}
