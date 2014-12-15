79576363581b32b2db7c86882b796d30403143e1
eddy5641 <eddy5641@gmail.com>
2014-12-15 00:38:02 -0800

    work later its 12:40

---
+++ LegendaryClient/Controls/GameScouter/ChatScouuterPlayer.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LegendaryClient.Controls.GameScouter
+{
+    /// <summary>
+    /// Interaction logic for ChatScouuterPlayer.xaml
+    /// </summary>
+    public partial class ChatScouuterPlayer : UserControl
+    {
+        /// <summary>
+        /// This is for curse voice + razer comms (Add chat into lc, with the scouter)
+        /// Designed for League so no worries about anything
+        /// </summary>
+        public ChatScouuterPlayer()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- LegendaryClient/GameScouter.xaml.cs
+++ LegendaryClient/GameScouter.xaml.cs
@@ -14,7 +14,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
+using LegendaryClient.Logic.PlayerSpell;
+using LegendaryClient.Logic.SQLite;
+using LegendaryClient.Controls.GameScouter;
+using LegendaryClient.Windows.Profile;
+using PVPNetConnect.RiotObjects.Platform.Statistics;
+using System.Reflection;
 
 namespace LegendaryClient
 {
@@ -29,7 +35,7 @@ namespace LegendaryClient
             Client.win = this;
             LoadScouter();
         }
-        public static void LoadScouter(string User = null)
+        public void LoadScouter(string User = null)
         {
             if (String.IsNullOrEmpty(User))
                 User = Client.LoginPacket.AllSummonerData.Summoner.Name;
@@ -50,17 +56,74 @@ namespace LegendaryClient
                 return true;
         }
 
-        private static async void LoadStats(string user)
+        private async void LoadStats(string user)
         {
             PlatformGameLifecycleDTO n = await Client.PVPNet.RetrieveInProgressSpectatorGameInfo(user);
             if (n.GameName != null)
             {
-
+                Load(new List<Participant>(n.Game.TeamOne.ToArray()), n, BlueTeam);
+                Load(new List<Participant>(n.Game.TeamTwo.ToArray()), n, PurpleTeam);
             }
             else
             {
                 Client.win.Visibility = Visibility.Hidden;
                 Client.win.Close();
+            }
+        }
+        List<MatchStats> GameStats = new List<MatchStats>();
+        private async void Load(List<Participant> allParticipants,PlatformGameLifecycleDTO n, ListView list)
+        {
+            foreach (Participant par in allParticipants)
+            {
+                if (par is PlayerParticipant)
+                {
+                    var participant = par as PlayerParticipant;
+                    foreach (PlayerChampionSelectionDTO championSelect in n.Game.PlayerChampionSelections.Where(championSelect =>
+                        championSelect.SummonerInternalName == participant.SummonerInternalName))
+                    {
+                        GameScouterPlayer control = new GameScouterPlayer();
+                        control.Username.Content = championSelect.SummonerInternalName;
+                        control.ChampIcon.Source = champions.GetChampion(championSelect.ChampionId).icon;
+                        var uriSource = new Uri(Path.Combine(Client.ExecutingDirectory, "Assets", "spell", SummonerSpell.GetSpellImageName(Convert.ToInt32(championSelect.Spell1Id))),
+                            UriKind.Absolute);
+                        control.SumIcon1.Source = new BitmapImage(uriSource);
+                        uriSource =
+                            new Uri(
+                                Path.Combine(Client.ExecutingDirectory, "Assets", "spell",
+                                    SummonerSpell.GetSpellImageName(Convert.ToInt32(championSelect.Spell2Id))),
+                                UriKind.Absolute);
+                        control.SumIcon2.Source = new BitmapImage(uriSource);
+                        GameStats.Clear();
+                        try
+                        {
+                            PublicSummoner summoner = await Client.PVPNet.GetSummonerByName(championSelect.SummonerInternalName);
+                            RecentGames result = await Client.PVPNet.GetRecentGames(summoner.AcctId);
+                            result.GameStatistics.Sort((s1, s2) => s2.CreateDate.CompareTo(s1.CreateDate));
+                            foreach (PlayerGameStats game in result.GameStatistics)
+                            {
+                                game.GameType =
+                                    Client.TitleCaseString(game.GameType.Replace("_GAME", "").Replace("MATCHED", "NORMAL"));
+                                var match = new MatchStats();
+
+                                foreach (RawStat stat in game.Statistics)
+                                {
+                                    Type type = typeof (MatchStats);
+                                    string fieldName = Client.TitleCaseString(stat.StatType.Replace('_', ' ')).Replace(" ", "");
+                                    FieldInfo f = type.GetField(fieldName);
+                                    f.SetValue(match, stat.Value);
+                                }
+                                match.Game = game;
+                                GameStats.Add(match);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+
+                        list.Items.Add(control);
+                    }
+                }
             }
         }
     }
