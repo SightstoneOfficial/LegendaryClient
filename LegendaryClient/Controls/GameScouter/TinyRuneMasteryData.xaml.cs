2d34603054585ac01840ea357afa80237826d76b
RoachxD <roachxxxx@gmail.com>
2014-12-19 11:24:27 +0200

    Fixed Errors

--- LegendaryClient/Controls/GameScouter/TinyRuneMasteryData.xaml.cs
+++ LegendaryClient/Controls/GameScouter/TinyRuneMasteryData.xaml.cs
@@ -1,37 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿#region
+
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+#endregion
 
 namespace LegendaryClient.Controls.GameScouter
 {
     /// <summary>
-    /// Interaction logic for TinyRuneMasteryData.xaml
+    ///     Interaction logic for TinyRuneMasteryData.xaml
     /// </summary>
-    public partial class TinyRuneMasteryData : UserControl
+    public partial class TinyRuneMasteryData
     {
         public TinyRuneMasteryData()
         {
             InitializeComponent();
         }
 
-        private void MouseEnter(object sender, MouseEventArgs e)
+        private new void MouseEnter(object sender, MouseEventArgs e)
         {
-
         }
-        private void Leave(object sender, MouseEventArgs e)
-        {
 
+        private new void MouseLeave(object sender, MouseEventArgs e)
+        {
         }
     }
 }
