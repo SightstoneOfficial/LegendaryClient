dbfc721dcb1dfbb0d048c2ec67d3cae92cf5acd8
eddy5641 <eddy5641@gmail.com>
2015-03-03 00:05:01 -0800

    Update for garena

--- LegendaryClient/Properties/AssemblyInfo.cs
+++ LegendaryClient/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using log4net.Config;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Legendary Coding")]
 [assembly: AssemblyProduct("LegendaryClient")]
-[assembly: AssemblyCopyright("Copyright (c) 2013-2015, Eddy5641/Snowl (Eddy V)")]
+[assembly: AssemblyCopyright("Copyright (c) 2013-2015, All devs at https://github.com/eddy5641/LegendaryClient and snowl")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -55,6 +55,6 @@ using log4net.Config;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.2.2.1")]
-[assembly: AssemblyFileVersion("2.2.2.1")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 [assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
