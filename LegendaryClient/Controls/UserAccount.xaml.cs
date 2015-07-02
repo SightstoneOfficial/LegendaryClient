bd5b35207d7478f88c08e9edc9623cfd067aae23
Martin Konstantin Herrmann <martin.k.herrmann@me.com>
2015-07-02 23:16:07 +0100

    Multiuser preps

---
+++ LegendaryClient/Controls/UserAccount.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LegendaryClient.Controls
+{
+    /// <summary>
+    ///     Interaction logic for UserAccount.xaml
+    /// </summary>
+    public partial class UserAccount
+    {
+        public UserAccount()
+        {
+            InitializeComponent();
+        }
+    }
+}
