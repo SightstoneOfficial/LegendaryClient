ad3880212daf63b175292e68608ec52d4bb554fa
nairaner <krzysztof.machura@wellner.pl>
2015-07-03 15:13:47 +0200

    this might fix it

--- LegendaryClient/Logic/Client.cs
+++ LegendaryClient/Logic/Client.cs
@@ -1197,6 +1197,8 @@ namespace LegendaryClient.Logic
                     string names = "";
                     try
                     {
+                        client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+                        client.Headers.Add("Content-Type", "text/html; charset=UTF-8");
                         names = client.DownloadString("http://legendaryclient.net/QueueName");
                     }
                     catch
