1d7006b126ac46d02f4ab3bd5b329d4e01bb0fe9
newchild <fabianbrand98@gmx.de>
2015-07-09 21:42:45 +0200

    Some stuff

--- LegendaryClient/Logic/Crypto/RSAWrapper.cs
+++ LegendaryClient/Logic/Crypto/RSAWrapper.cs
@@ -6,11 +6,55 @@ using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Prng;
 using Org.BouncyCastle.Security;
+using System.Text;
+using System.Windows.Forms;
 
 namespace LegendaryClient.Logic.Crypto
 {
 	class ChatEncryptionWrapper
 	{
+		public static enum ExchangeResult
+		{
+			Success,
+			PartnerHasNoKey,
+			FailedUnknown
+		}
+		public static ExchangeResult exchangeKeys(string otherUser)
+		{
+			if (ChatEncryptionInfo.keyPair == null)
+			{
+				return ExchangeResult.FailedUnknown;
+			}
+			//ToDo: Add key exchanging, maybe via Chat?
+
+		}
+
+		public static void LoadKeys()
+		{
+
+			var file = File.OpenRead(Path.Combine(Client.ExecutingDirectory, "Crypto", "priv.key"));
+			ChatEncryptionInfo.keyPair.Private = file;
+			file.Close();
+			file = File.OpenRead(Path.Combine(Client.ExecutingDirectory, "Crypto", "pub.key"));
+			ChatEncryptionInfo.keyPair.Public = file;
+			file.Close();
+		}
+
+
+		public static void SaveKeys()
+		{
+			var file = File.OpenWrite(Path.Combine(Client.ExecutingDirectory, "Crypto", "priv.key"));
+			var memStream = new MemoryStream(ChatEncryptionInfo.keyPair.Private);
+			file.Write(memStream.ToArray(), 0, memStream.Length);
+			file.Close();
+			memStream.Close();
+			var file = File.OpenWrite(Path.Combine(Client.ExecutingDirectory, "Crypto", "pub.key"));
+			var memStream = new MemoryStream(ChatEncryptionInfo.keyPair.Public);
+			file.Write(memStream.ToArray(), 0, memStream.Length);
+			file.Close();
+			memStream.Close();
+		}
+
 		public static void generateKeys()
 		{
 			var keyPairGenerator = new RsaKeyPairGenerator();
@@ -64,10 +108,32 @@ namespace LegendaryClient.Logic.Crypto
 			}
 			return System.Text.Encoding.Unicode.GetString(correctBytes.ToArray());
 		}
+
+		public static string EncryptString(string message, PgpPublicKey PublicKey)
+		{
+			var outStream = new MemoryStream();
+			PgpLiteralDataGenerator lData = new PgpLiteralDataGenerator();
+			var pOut = lData.Open(outStream, PgpLiteralData.Binary, PgpLiteralData.CONSOLE, Encoding.Default.GetBytes(message).Length, DateTime.Now);
+			pOut.Write(Encoding.Default.GetBytes(message), 0, Encoding.Default.GetBytes(message).Length);
+			lData.Close();
+			PgpEncryptedDataGenerator cPK = new PgpEncryptedDataGenerator(Org.BouncyCastle.Bcpg.SymmetricKeyAlgorithmTag.Aes256, false, new SecureRandom());
+			cPK.AddMethod(PublicKey);
+			var x = cPK.Open(outStream, outStream.ToArray());
+			var MemPOut = new MemoryStream();
+			pOut.CopyTo(MemPOut);
+			x.Write(MemPOut.ToArray(), 0, MemPOut.Length);
+			var encryptedBytes = (x as MemoryStream).ToArray();
+			return Encoding.Default(encryptedBytes);
+		}
 		
 	}
+
 	class ChatEncryptionInfo
 	{
+		/// <summary>
+		/// Stores all known Keys
+		/// </summary>
+		public static Dictionary<string, PgpKeyPair> Keys = new Dictionary<string, PgpKeyPair>();
 		public static AsymmetricCipherKeyPair keyPair = null;
 
 	}
