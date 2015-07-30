e255a0553304a4c744024993f037c8585e81462e
Jittapan Pluemsumran <foreverfk@stephack.com>
2015-07-30 18:35:09 +0700

    Dev button for screenshot viewer

--- Sightstone/Windows/ScreenshotsPage.xaml.cs
+++ Sightstone/Windows/ScreenshotsPage.xaml.cs
@@ -2,6 +2,7 @@
 using Sightstone.Logic;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.IO;
 using System.Net;
@@ -38,30 +39,52 @@ namespace Sightstone.Windows
         }
 
         private string ScreenshotFolder;
+        private UserClient user;
 
         public ScreenshotsPage()
         {
             InitializeComponent();
+            Load();
         }
 
         private void DevButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!user.Dev) return;
             Client.OverlayContainer.Content = new ScreenshotViewer().Content;
             Client.OverlayContainer.Visibility = Visibility.Visible;
         }
 
         private void Load()
         {
-            UserClient user = (UserList.Users[Client.CurrentServer])[Client.CurrentUser];
+            user = (UserList.Users[Client.CurrentServer])[Client.CurrentUser];
             if(user.Garena)
             {
                 ScreenshotFolder = Path.Combine(Client.Location, "screenshots");
+                ScreenshotsPath.Text = ScreenshotFolder;
+                if(!user.Dev)
+                {
+                    DevButton.Visibility = Visibility.Hidden;
+                }
             }
             else
             {
                 // TODO : Add logic for non-Garena servers.
             }
             // TODO : Add control for each screenshot
+        }
+
+        private void OpenScreenshotsPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Directory.Exists(ScreenshotFolder))
+            {
+                Process.Start(ScreenshotFolder);
+            }
+            else
+            {
+                var overlay = new MessageOverlay { MessageTitle = { Content = "Error" }, MessageTextBox = { Text = ScreenshotFolder + " does not exist." } };
+                Client.OverlayContainer.Content = overlay.Content;
+                Client.OverlayContainer.Visibility = Visibility.Visible;
+            }
         }
     }
 }
